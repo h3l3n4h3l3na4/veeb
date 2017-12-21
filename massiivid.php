8ad7afdacd20c9233f26819ef26744d770de5995
helena hass <helena.hass@khk.ee>
2017-12-21 14:34:34 +0200

    Massiivi test ja korralik väljastus

--- massiivid.php
+++ massiivid.php
@@ -5,7 +5,6 @@
  * Date: 21.12.2017
  * Time: 14:20
  */
-
 $arvud = array(); // tühi massiiv
 // lisame väärtused
 $arvud[] = 5;
@@ -16,3 +15,17 @@ $arvud[] = 3;
 
 // kuidas luua juba väärtustega massiivi
 $arvud1 = array(1, 2, 3, 4, 5);
+
+// testväljastus
+echo '<pre>';
+print_r($arvud);
+echo '</pre>';
+
+// korralik väljastus
+for ($i = 0; $i < count($arvud); $i++){
+    echo '<b>'.$arvud[$i].'</b><br />';
+}
+
+foreach ($arvud1 as $arv){
+    echo '<i>'.$arv.'</i><br />';
+}
