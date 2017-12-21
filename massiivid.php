8163f2eefd961379067bb0e58b67eafe1be001a3
helena hass <helena.hass@khk.ee>
2017-12-21 15:29:55 +0200

    Ülesanne 3 lahendus

--- massiivid.php
+++ massiivid.php
@@ -91,7 +91,23 @@ echo '<hr />';
  * Kontrolli kas kohad on vahetatud kasutades
  * valjastaMassiiv funktsiooni
 */
+function vahetaMinMax(&$massiiv){
+    $min = min($massiiv);
+    $max = max($massiiv);
+    echo 'Minimum = '.$min.'<br />';
+    echo 'Maximum = '.$max.'<br />';
+    for ($i = 0; $i < count($massiiv); $i++){
+        if ($massiiv[$i] == $min){
+            $massiiv[$i] = $max;
+        }elseif ($massiiv[$i] == $max){
+            $massiiv[$i] = $min;
+        }
+        echo $massiiv[$i].'<br />';
+    }
+}
 
+vahetaMinMax($testmassiiv);
+valjastaMassiiv($testmassiiv);
 /*
  * 4.
  * Loo funktsioon nimega elementideKorrutis, mis
